d2b77c34c39f4a58401f2c2908f69faf157b5658
Ilshat <110959745+Ftr4vis@users.noreply.github.com>
2024-04-16 12:43:00 +0300

    Update waf_init.lua

--- waf_init.lua
+++ waf_init.lua
@@ -1,9 +1,6 @@
-local http = require "resty.http"
-local redis = require "resty.redis"
-
 local waf_init = {}
 
-waf_init.waf_mode = "On"
+waf_init.waf_mode = "Off"
 waf_init.score_threshold = 8
 
 local available_modes = {}
@@ -21,10 +18,16 @@ if waf_init.score_threshold < 0 then
     waf_init.score_threshold = 10
 end
 
+waf_init.allowed_methods = {}
+waf_init.allowed_methods['GET'] = true
+waf_init.allowed_methods['POST'] = true
+waf_init.allowed_methods['HEAD'] = true
+waf_init.allowed_methods['OPTIONS'] = true
+
 if waf_init.waf_mode ~= "Off" then
     waf_init.rulesets = dofile("/usr/local/openresty/luawaf/get_rules.lua")
-    waf_init.user_agents_blacklist = dofile("/usr/local/openresty/luawaf/user_agents/user_agents_blacklist.lua")
-    waf_init.ip_blacklist = dofile("/usr/local/openresty/luawaf/ip/ip_blacklist.lua")
+    waf_init.user_agents_scanners = dofile("/usr/local/openresty/luawaf/user_agents/user_agents_scanners.lua")
+    waf_init.torvpn_ip_list = dofile("/usr/local/openresty/luawaf/ip/torvpn_ip_list.lua")
 end
 
 return waf_init
