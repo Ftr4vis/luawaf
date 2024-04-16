9967cdb583c99c8d2abf4e3b949f6c33e49194b4
Ilshat <110959745+Ftr4vis@users.noreply.github.com>
2024-04-16 12:36:46 +0300

    Update rules_wp.lua

--- rules/rules_wp.lua
+++ rules/rules_wp.lua
@@ -1,3 +1,3 @@
 rules_wp = {}
-rules_wp['wordpress_sensitive_files'] = {regex = "(wp\\-cron)|(wp\\-config)\\.php", score = 5}
+rules_wp['wordpress_sensitive_files'] = {regex = "(wp\\-cron)|(wp\\-config)|(install)|(version)|(xmlrpc)\\.php", score = 50}
 return rules_wp
