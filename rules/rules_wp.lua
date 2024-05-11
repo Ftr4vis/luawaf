f572fb32dd5abbc06ffbac4bb90855f0b689b236
Ilshat <110959745+Ftr4vis@users.noreply.github.com>
2024-05-11 12:41:15 +0300

    Update rules_wp.lua

--- rules/rules_wp.lua
+++ rules/rules_wp.lua
@@ -1,3 +1,5 @@
 rules_wp = {}
-rules_wp['wordpress_sensitive_files'] = {regex = "(wp\\-cron)|(wp\\-config)|(install)|(version)|(xmlrpc)\\.php", score = 50}
+rules_wp['wordpress_sensitive_files'] = {regex = "(wp\\-cron|wp\\-config|install|version|xmlrpc)\\.php", score = 50}
+rules_wp['wordpress_admin_files'] = {regex = "(admin\\-post|admin\\-ajax|admin\\-footer|admin\\-functions|admin\\-header|admin)\\.php", score = 50}
+rules_wp['wordpress_common_dirs_checked'] = {regex = "(\\/backup\\/|\\/blog\\/|\\/cms\\/|\\/demo\\/|\\/dev\\/|\\/home\\/|\\/main\\/|\\/new\\/|\\/old\\/|\\/portal\\/|\\/site\\/|\\/test\\/|\\/tmp\\/|\\/web\\/|\\/wordpress\\/|\\/wp\\/)", score = 50}
 return rules_wp
